6296231bafcd24b0e0bcf67f9457ca1f669fdd6b
za0012 <zinzeek@gmail.com>
2024-12-08 17:29:11 +0900

    fix: 오탈자 수정 main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import * as Sentry from '@Sentry/react';
+import * as Sentry from '@sentry/react';
 
 const queryClient = new QueryClient();
 
